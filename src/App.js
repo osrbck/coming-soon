fb070b4aa6f6755592b7397c160880b6029f3e68
Macintoshman <osrbck@gmail.com>
2024-09-29 15:59:57 +0300

    cards added and fixed

--- src/App.js
+++ src/App.js
@@ -4,6 +4,10 @@ import '@leenguyen/react-flip-clock-countdown/dist/index.css';
 import './App.css';
 import logo from './assets/logo.svg'; // Import your background image
 import Subscribe from './components/Subscribe';
+import Card from './components/Card';
+import { FaFigma , FaCode, FaSketch } from 'react-icons/fa';
+
+
 
 function App() {
   return (
@@ -15,13 +19,17 @@ function App() {
       <div className="clock_content">
         <img src={logo} alt="Logo" className="logo" />
         <h1>We are almost there!</h1>
-        <h3>Stay tuned for something amazing.</h3>
         <FlipClockCountdown
           className="flip-clock"
           to={new Date().getTime() + 301 * 24 * 3600 * 1000}  // 365 days in milliseconds
-          labels={["DAYS", "HOURS", "MINUTES", "SECONDS"]}
+          labels={["DAYS", "HOURS", "MINS", "SECS"]}
           duration={0.5}
         />
+        <div className="card-container">
+          <Card name="Design" icon={<FaFigma size={50} color="#E6D7B3" />} />
+          <Card name="Develop" icon={<FaCode size={50} color="#D98D30" />} />
+          <Card name="Brand" icon={<FaSketch size={50} color="#D96E30" />} />
+        </div>
         <Subscribe />
       </div>
     </section>
